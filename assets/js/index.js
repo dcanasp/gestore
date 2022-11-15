79021cea892a40edbacaed3dcfc16d02a9086740
david cañas <dcanasp@unal.edu.co>
2022-11-14 23:47:30 -0500

    imagenes primer paso

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,7 @@
-console.log("si");
+console.log("INICIO");
 const productos= async () =>{
-
-    let url = 'http://localhost:3000/getAllProducts';
-    let datos;
+let url = 'http://localhost:3000/getAllProducts';
+let datos;
 const x = await fetch(url, {
     method : "GET",
     mode: 'cors',
@@ -13,8 +12,27 @@ let products=[];
 for (let i=0;i<15;i++){
     products.push(datos[i]);
 }
-//tu funcion
-console.log(products);
+console.log("funciono?");
+let imagenes = getImages(products);
+console.log(imagenes);
 }
-
 productos();
+
+const getImages = async (products) => {
+let url = 'http://localhost:3000/getAllImages';
+let datos;
+const x = await fetch(url, {
+    method : "GET",
+    mode: 'cors',
+    cache: 'no-cache',
+    }).then(response => response.json()).then(data => datos=data);
+
+let imagenes=[];
+console.log("aqui");
+console.log(datos);
+products.forEach(prod => {
+    imagenes.push(datos[prod.image_id].image);
+});
+
+
+}
