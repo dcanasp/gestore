e9095ee32500d87e921eb10b17067850d54c6910
Solnarux <eslopezb@unal.edu.co>
2022-11-15 23:51:17 -0500

    avances mis productos

---
+++ assets/js/my-products.js
@@ -0,0 +1,77 @@
+const productos= async () =>{
+    let url = 'http://localhost:3000/getAllProducts';
+    let datos;
+    const x = await fetch(url, {
+        method : "GET",
+        mode: 'cors',
+        cache: 'no-cache',
+        }).then(response => response.json()).then(data => datos=data);
+
+    let products=[];
+    for (let i=0;i<16;i++){
+        if(datos[i].user_id == 1){
+            products.push(datos[i]);
+        } 
+    }
+    let imagenes = await getImages(products);
+
+    let padre = document.getElementById("inicio");
+    let contador =0;
+    products.forEach(prod => {
+        let texto = creacion(prod,imagenes[contador]);
+        padre.innerHTML = padre.innerHTML + texto;
+        //padre.innerHTML = texto + padre.innerHTML;// por si lo quiero alrevez
+        padre.parentNode.insertBefore(padre, padre);
+        contador ++;
+    })
+    console.log(padre);
+
+    padre.addEventListener("click", prueba, false);
+    return;
+}
+
+const creacion = (product,imagen) =>{
+    let x = `
+    <div class="col-lg-4 col-md-6 portfolio-item">
+    <div class="portfolio-wrap">
+        <img src="${imagen}" class="img-fluid portfolio-lightbox" alt="">
+        <div class="portfolio-info">
+        </div>
+    </div>            
+    <p class="price">\$${product.precio}</p>
+    <a href="portfolio-details.html" title="More Details" class="box box-link" id="${product.product_id}">${product.nombre}</a>
+    </div>
+    `
+      return x;
+  }
+
+
+
+const getImages = async (products) => {
+    let url = 'http://localhost:3000/getAllImages';
+    let datos;
+    const x = await fetch(url, {
+        method : "GET",
+        mode: 'cors',
+        cache: 'no-cache',
+        }).then(response => response.json()).then(data => datos=data);
+
+    let imagenes=[];
+    products.forEach(prod => {
+        imagenes.push(datos[prod.image_id].image);
+    });
+    return imagenes;
+
+}
+
+function prueba(e){
+    console.log(e);
+    if(e.target !== e.currentTarget){
+        let clicked_e = e.target.id;
+        localStorage.setItem("product_id", clicked_e)
+    }
+    // let prod_serialized = JSON.stringify(prod);
+    // localStorage.setItem(prod.nombre + prod.product_id,prod_serialized);
+}
+    
+productos();
---
+++ assets/js/product-details-seller.js
@@ -0,0 +1,93 @@
+const producto = async () =>{
+  let prod_id = localStorage.getItem("product_id");
+  if (prod_id==undefined){
+    console.log("no product_id")
+    return "error"
+  }
+  let url = 'http://localhost:3000/getProduct/'+prod_id;
+  let datos;
+  const x = await fetch(url, {
+      method : "GET",
+      mode: 'cors',
+      cache: 'no-cache',
+      }).then(response => response.json()).then(data => datos=data);
+
+  let imagen = await getImages(datos);
+  let padre = document.getElementById("detalleProducto");
+  let texto = creacion(datos, imagen);
+  padre.innerHTML = padre.innerHTML + texto;
+  //padre.innerHTML = texto + padre.innerHTML;// por si lo quiero alrevez
+  padre.parentNode.insertBefore(padre, padre);
+};
+    
+
+
+const creacion = (product,imagen) =>{
+    let x = `
+    <!-- ======= Portfolio Details Section ======= -->
+    <section id="portfolio-details" class="portfolio-details">
+      <div class="container">
+
+        <div class="row gy-4">
+
+          <div class="col-lg-8">
+            <div class="portfolio-details-slider swiper">
+              <div class="swiper-wrapper align-items-center">
+
+                <div class="swiper-slide">
+                  <img src="${imagen}" alt="">
+                </div>
+
+                <div class="swiper-slide">
+                  <img src="assets/img/portfolio/portfolio-2.jpg" alt="">
+                </div>
+
+                <div class="swiper-slide">
+                  <img src="assets/img/portfolio/portfolio-3.jpg" alt="">
+                </div>
+
+              </div>
+              <div class="swiper-pagination"></div>
+            </div>
+          </div>
+
+          <div class="col-lg-4">
+            <div class="portfolio-info">
+              <h3>Informacion del producto</h3>
+              <ul>
+                <li><strong>Categoria</strong>: ${product.categoria}</li>
+                <li><strong>Vendedor</strong>: ${product.user_id}</li>
+                <li><strong>Precio</strong>: ${product.precio}</li>
+                <li><strong>Stock</strong>: ${product.stock}</li>
+              </ul>
+            </div>
+            <div class="portfolio-description">
+              <h2>Descripcion</h2>
+              <p>
+                ${product.descripcion}
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Details Section -->
+    `
+      return x;
+  }
+
+
+
+const getImages = async (product) => {
+    let url = 'http://localhost:3000/getImages/' + product.image_id;
+    let datos;
+    const x = await fetch(url, {
+        method : "GET",
+        mode: 'cors',
+        cache: 'no-cache',
+        }).then(response => response.text()).then(data => datos=data);
+    return datos;
+}
+    
+producto();
