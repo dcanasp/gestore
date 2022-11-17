7085843cc17caa9e7a9b129d08a413e7e242d52d
jsarmientopu <jsarmientopu@unal.edu.co>
2022-11-16 23:20:07 -0500

    merge 2.0

--- assets/js/registrar.js
+++ assets/js/registrar.js
@@ -2,7 +2,6 @@ const { htmlPrefilter } = require("jquery");
 
 const registro = async () => {
   let token;
-<<<<<<< HEAD
   let username = document.getElementById('username').value;
   let password = document.getElementById('password').value;
   let email = document.getElementById('email').value;
@@ -40,34 +39,3 @@ console.log(JSON.stringify(body));
 }
 
 document.getElementById('botonLoginAYUDA').addEventListener('click', registro, false );
-=======
-  let username = document.getElementsById('username');
-  let password = document.getElementsById('password');
-  let email = document.getElementsById('email');
-  let rol = document.getElementsById('rol');
-  //2 vendedor 
-  //1 cliente 
-  if (rol == true) {
-
-    rol = 2
-  } else {
-
-    rol = 1
-  }
-
-
-}
-const x = await fetch('http://localhost:3000/createUser', {
-  method: "POST",
-  mode: 'cors',
-  cache: 'no-cache',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(body),
-}).then(response => response.json()).then(data => token = data);
-console.log(token);
-window.localStorage.setItem("token", token.token);
-
-let z = localStorage.getItem('token');
->>>>>>> ff311774c0b9f663d58a6a682ef577dcccad6cbb
