f0a703888680739d8446e8b3e61c20e94e9766d6
Cristhian Cely <cristhiancely7@gmail.com>
2022-11-25 18:19:19 -0500

    agregue archivos js y css para poder ver los productos agregados al carrito desde el navbar, funciona

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -4,9 +4,38 @@ class Carrito {
         e.preventDefault();
         if(e.target.classList.contains('agregar-carrito')){
             const producto = e.target.parentElement.parentElement;
-            // this.leerDatosProducto(producto);
-
-            console.log(producto);
+            this.leerDatosProducto(producto);
         }
     }
+
+    leerDatosProducto(producto){
+        const infoProducto = {
+            imagen : producto.querySelector('img').src,
+            titulo : producto.querySelector('h4').textContent,
+            precio : producto.querySelector('.precio span').textContent,
+            id : producto.querySelector('a').getAttribute('data-id'),
+            cantidad : 1
+        }
+        this.insertarCarrito( infoProducto );
+    }
+
+    insertarCarrito(producto){
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                <img src="${producto.imagen}" width=100>
+            </td>
+            <td>${producto.titulo}</td>
+            <td>${producto.precio}</td>
+            <td>
+                <a href="#" class="borrar-producto fas fa-times-circle data-id="${producto.id}"></a>
+            </td>
+        `;
+        listaProductos.appendChild(row);
+    }
+
+
 }
+
+
+
