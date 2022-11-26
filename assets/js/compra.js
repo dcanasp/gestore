8d59fa458a362a1029e6275161a2b69c56ef9354
Cristhian Cely <cristhiancely7@gmail.com>
2022-11-26 00:09:26 -0500

    se creo funcion procesar compra

--- assets/js/compra.js
+++ assets/js/compra.js
@@ -1,5 +1,9 @@
 const compra = new Carrito();
 const listaCompra = document.querySelector('#lista-compra tbody');
+const carrito = document.getElementById('carrito');
+const procesarCompraBtn = document.getElementById('procesar-comrpra');
+const cliente = document.getElementById('cliente');
+const correo = document.getElementById('correo');
 
 cargarEventos();
 
@@ -7,4 +11,18 @@ function cargarEventos(){
     document.addEventListener('DOMContentLoaded', compra.leerLocalStorageCompra());
 
     compra.calcularTotal();
+
+    procesarCompraBtn.addEventListener('click', procesarCompra);
+
+}
+
+function procesarCompra(){
+    e.preventDefault();
+
+    if(cliente.value === '' || correo.value === ''){
+        window.alert('Ingresa todos los campos requeridos');
+    }else{
+        const cargandoGif = document.querySelector('#cargando');
+        cargandoGif.style.display = 'block';
+    }
 }
