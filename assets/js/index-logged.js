0fb04baaeee50ef46d215f61a48b0633722f422d
jsarmientopu <jsarmientopu@unal.edu.co>
2022-12-01 11:26:22 -0500

    arreglo login

--- assets/js/index-logged.js
+++ assets/js/index-logged.js
@@ -5,8 +5,7 @@ const productos= async () =>{
       method : "GET",
       mode: 'cors',
       cache: 'no-cache',
-      }).then(response => response.text()).then(data => datos=data);
-      if(response != 'Algo salio mal'){
+      }).then(response => response.json()).then(data => datos=data);
         let products=[];
         for (let i=0;i<datos.length;i++){
             products.push(datos[i]);
@@ -24,7 +23,7 @@ const productos= async () =>{
 
         padre.addEventListener("click", prueba, false);
         return;
-    }
+
 }
 
 const creacion = (product,imagen) =>{
--- assets/js/login.js
+++ assets/js/login.js
@@ -2,16 +2,19 @@ const login = async () =>{//si usuario y clave correctos, MAL NO USAR
     let password = document.getElementById('password').value;
     let username = document.getElementById('username').value;
     let url = 'http://localhost:3000/checkUser/'+username+'?password='+password;
+    console.log(url);
       let datos;
       const x = await fetch(url, {
         method : "GET",
         mode: 'cors',
         cache: 'no-cache'
       }
-      ).then(response => response.text()).then(data => datos=data);
+      ).then(response => response.json()).then(data => datos=data);
       if(datos.token!="Algo salio mal"){
         localStorage.setItem('token',datos.token);
         window.location.replace("http://localhost:1234/index-logged.html");
+      }else{
+        //CREDENCIALES INCORRECTAS
       }
 }
 
