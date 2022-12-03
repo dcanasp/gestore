16b30cf5a6ef79a6967f7594302c3c1661880388
david cañas <dcanasp@unal.edu.co>
2022-12-03 09:03:44 -0500

    commit deploy final

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,7 +3,7 @@ const { default: Swal } = require("sweetalert2");
 const login = async () =>{//si usuario y clave correctos, MAL NO USAR
     let password = document.getElementById('password').value;
     let username = document.getElementById('username').value;
-    let url = 'http://localhost:3000'+'/checkUser/'+username+'?password='+password;
+    let url = 'http://ec2-52-91-104-218.compute-1.amazonaws.com:1234'+'/checkUser/'+username+'?password='+password;
       let datos;
       const x = await fetch(url, {
         method : "GET",
